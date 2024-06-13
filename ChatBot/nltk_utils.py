e12e1737f33f38815e9866f020b4ed5976cc94bb
Nikhil394 <119506539+Nikhil394@users.noreply.github.com>
2024-06-13 18:01:27 +0530

    Update nltk_utils.py

--- ChatBot/nltk_utils.py
+++ ChatBot/nltk_utils.py
@@ -1,43 +1,19 @@
 import numpy as np
 import nltk
-# nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
-stemmer = PorterStemmer()
 
-def tokenize(sentence):
-    """
-    split sentence into array of words/tokens
-    a token can be a word or punctuation character, or number
-    """
-    return nltk.word_tokenize(sentence)
+porter = PorterStemmer()
 
+def tokenize_text(text):
+    return nltk.word_tokenize(text)
 
-def stem(word):
-    """
-    stemming = find the root form of the word
-    examples:
-    words = ["organize", "organizes", "organizing"]
-    words = [stem(w) for w in words]
-    -> ["organ", "organ", "organ"]
-    """
-    return stemmer.stem(word.lower())
+def stem_word(word):
+    return porter.stem(word.lower())
 
-
-def bag_of_words(tokenized_sentence, words):
-    """
-    return bag of words array:
-    1 for each known word that exists in the sentence, 0 otherwise
-    example:
-    sentence = ["hello", "how", "are", "you"]
-    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
-    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
-    """
-    # stem each word
-    sentence_words = [stem(word) for word in tokenized_sentence]
-    # initialize bag with 0 for each word
-    bag = np.zeros(len(words), dtype=np.float32)
-    for idx, w in enumerate(words):
-        if w in sentence_words: 
-            bag[idx] = 1
-
-    return bag
+def generate_bag_of_words(tokenized_sentence, vocabulary):
+    stemmed_words = [stem_word(word) for word in tokenized_sentence]
+    bag_of_words = np.zeros(len(vocabulary), dtype=np.float32)
+    for idx, word in enumerate(vocabulary):
+        if word in stemmed_words:
+            bag_of_words[idx] = 1
+    return bag_of_words
